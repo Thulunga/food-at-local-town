74fb9525a47ced0e7881ac8be6e52e8e756c18f9
Thulunga <thulunga.tb@gmail.com>
2022-01-31 20:03:55 +0530

    dynamic product page added

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -3,20 +3,31 @@ import Product from "../../../modals/Product";
 import dbConnect from "../../../util/mongo";
 
 export default async function handler(req, res) {
-  const {method} = req;
+  const {method, query:{id},} = req;
 
   dbConnect()
   
 
   if(method === "GET") {
     try {
-      const product = await Product.find();
+      const product = await Product.findById(id);
       res.status(200).json(product)
     } catch (error) {
       res.status(500).json(error)
     }
   }
-  if(method === "POST") {
+//   put method is not corrected
+  if(method === "PUT") {
+    try {
+      const product = await Product.create(req.body);
+      res.status(201).json(product)
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+
+//   delete method is not corrected
+  if(method === "DELETE") {
     try {
       const product = await Product.create(req.body);
       res.status(201).json(product)
